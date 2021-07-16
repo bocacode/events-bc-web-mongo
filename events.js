2303d5e798e1a083bd0c70e22f80ec34c22bb53c
chrisdeleonn <cdizzel@ymail.com>
2021-07-16 15:25:41 -0400

    created addEvents branch

--- events.js
+++ events.js
@@ -2,21 +2,22 @@ const menuElement = document.querySelector('#menu')
 
 // fetch('http://52.90.190.236:5000/customers')
 fetch('http://52.90.190.236:5000/events')
-  .then(response => response.json())
+  .then((response) => response.json())
   // .then(data => console.log(data))
-  .then(menuItems => {
+  .then((menuItems) => {
     console.log(menuItems)
-    const menuCards = menuItems.map(event => {
+    const menuCards = menuItems.map((event) => {
       console.log(event)
 
       return `<div class="menu-item">
-          <h3>${event.title} </h3>
-          <p></p>
-          <p>${event.description}</p>
-          <p>${event.cost}</p>
+          <h3> Title: ${event.title} </h3>
+          <p> Date: ${event.date}</p>
+          <p> Description: ${event.description}</p>
+          <p> Cost: ${event.cost}</p>
+          <p> Attendees: ${event.attendees}</p>
         </div>`
     })
     menuElement.innerHTML = menuCards.join('')
   })
 
-  .catch(error => console.error(error))
+  .catch((error) => console.error(error))
