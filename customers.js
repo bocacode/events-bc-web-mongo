3a5146cdafc6c02591946d1c19fe1195a30cae13
NoahAlbert <no.albert113@gmail.com>
2021-07-16 15:19:22 -0400

    reformatted customers page

--- customers.js
+++ customers.js
@@ -1,7 +1,7 @@
 const menuElement = document.querySelector('#menu')
 
-// fetch('http://52.90.190.236:5000/customers')
-fetch('http://localhost:5000/customers')
+fetch('http://52.90.190.236:5000/customers')
+// fetch('http://localhost:5000/customers')
   .then(response => response.json())
   // .then(data => console.log(data))
   .then(customers => {
@@ -11,9 +11,10 @@ fetch('http://localhost:5000/customers')
 
       return `<div class="menu-item">
           <h3>${customer.fName} ${customer.lName}</h3>
-          <p></p>
-          <p>${customer.email}</p>
-          <p>${customer.phone}</p>
+          <br>
+          <p><b>email:</b> ${customer.email}</p>
+          <p><b>phone:</b> ${customer.phone}</p>
+          <br>
         </div>`
     })
     menuElement.innerHTML = menuCards.join('')
